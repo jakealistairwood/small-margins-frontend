0b5bed67b3e8c854a1948bda2042c5b23b32b3ce
Jake Wood <jakealistairwood@gmail.com>
2021-06-17 10:48:18 +0100

    saving changes

--- components/Shoe/Shoe.js
+++ components/Shoe/Shoe.js
@@ -1,17 +1,20 @@
 import styles from './Shoe.module.scss'
+import Link from 'next/link'
 
 const Shoe = ({ shoe }) => {
     return (
-        <div className={styles.shoe}>
-            <div className={styles.shoeImg}>
-                <img src={shoe.img} alt={`${shoe.brand} ${shoe.model}`} />
-            </div>
-            <div className={styles.shoeDetails}>
-                <h3>{shoe.brand} {shoe.model}</h3>
-                <p>{shoe.description.slice(0, 150)}...</p>
-                <p>£{shoe.price}</p>
-            </div>
-        </div>
+        <Link href="#">
+            <a className={styles.shoe}>
+                <div className={styles.shoeImg}>
+                    <img src={shoe.img} alt={`${shoe.brand} ${shoe.model}`} />
+                </div>
+                <div className={styles.shoeDetails}>
+                    <h3>{shoe.brand} {shoe.model}</h3>
+                    <p>{shoe.description.slice(0, 150)}...</p>
+                    <p>£{shoe.price}</p>
+                </div>
+            </a>
+        </Link>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,43 @@ import Shoe from '../components/Shoe'
 export default function Home({ shoes }) {
 
   return (
-    <Layout>
-      <div className={styles.banner}>
-        <div className={styles.bannerContent}>
-          {/* <h1>Supercharge Your Efforts</h1> */}
+    <>
+      <Layout>
+        <div className={styles.banner}>
+          <div className={styles.bannerContent}>
+            {/* <h1>Supercharge Your Efforts</h1> */}
+          </div>
         </div>
-      </div>
-      <section className={styles.trending}>
+        <section className={styles.trending}>
+          <div className={styles.sectionHeader}>
+            <h2>Trending Now</h2>
+            <div className={styles.designDetail}></div>
+          </div>
+          <div className={styles.gridContainer}>
+            {shoes.map((shoe) => (
+              <Shoe shoe={shoe} />
+            ))}
+          </div>
+        </section>
+      </Layout>
+      <section className={styles.articles}>
         <div className={styles.sectionHeader}>
-          <h2>Trending Now</h2>
+          <h2>Latest Articles</h2>
           <div className={styles.designDetail}></div>
         </div>
-        <div className={styles.gridContainer}>
-          {shoes.map((shoe) => (
-            <Shoe shoe={shoe} />
-          ))}
-        </div>
+        <div className={styles.articleContainer}>
+          <div className={`${styles.article} ${styles.article1}`}>
+            <h2>5 exercises to beat IT Band Syndrome</h2>
+          </div>
+          <div className={`${styles.article} ${styles.article2}`}>
+            <h2>Our beginner-friendly guide to running</h2>
+          </div>
+          <div className={`${styles.article} ${styles.article3}`}>
+            <h2>Top 10 health benefits of running</h2>
+          </div>
+        </div> 
       </section>
-    </Layout>
+    </>
   )
 }
 
